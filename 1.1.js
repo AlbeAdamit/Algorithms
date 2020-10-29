6dc69f1818d7a12a79a78559c5e84b76d46341fa
Alexandre Albelice <albelice.alex@gmail.com>
2020-10-29 16:09:51 +0100

    ExoAlgo1.3

---
+++ 1.1.js
@@ -0,0 +1,2 @@
+let name ="Alex";
+console.log(name);
---
+++ 1.2.js
@@ -0,0 +1,4 @@
+let name ="Gerard";
+let firstName ="Lambert";
+let city = "Bruxelles";
+console.log("Your name is "+ name +" "+firstName+" and you live in "+city);
